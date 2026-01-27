fda66c91a1fa7359d8b63788e9871ed7318c168b
StarkMist111960 <starkmist111960@gmail.com>
2026-01-27 09:36:53 -0700

    Implement user confirmation for RootFS disabling
    Added a user prompt to confirm disabling RootFS verification and updated commands to include device specification.

--- OctagonRootfs.sh
+++ OctagonRootfs.sh
@@ -26,8 +26,23 @@ echo "[*] Disabling RootFS verification..."
 echo ""
 
 # Disable RootFS verification on both partitions
-/usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 2
-/usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 4
+#!/bin/bash
+
+echo 'Welcome to the Pentagon Policy Editor RootFS Disabler!'
+read -p 'Disable RootFS verification (y, n)? ' ANS
+
+if [[ $ANS -eq 'y' ]]; then 
+
+sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
+sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
+echo 'RootFS has been disabled! (or already was)'
+
+else 
+  exit 1
+
+fi 
+
+echo 'if you recieve an error saying something like "cannot find required file /dev/mmcblk0" you may need to download or open this file and change the command to match your specific needs, thank you for using Pentgon Policy Editor RootFS disable'
 
 echo ""
 echo "[✓] RootFS verification disabled!"
